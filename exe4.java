23c72d7044669accddb2146803b611bcc8d67d42
Jean <112350318+JeanSousa6@users.noreply.github.com>
2024-02-26 17:05:42 -0300

    Add files via upload

---
+++ exe4.java
@@ -0,0 +1,36 @@
+/* 4. Escreva um programa que leia um número inteiro n e mostre na tela os n primeiros termos da sequência de Fibonacci. Essa
+sequência tem os dois primeiros termos 0 e 1, para os demais, cada termo é a soma dos dois anteriores.
+Exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, etc...  */
+
+package lista2.Banin.DispMov;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class exe4{
+     public static void main(String[] args) {
+
+        Scanner lerNum = new Scanner(System.in);
+        ArrayList<Integer> numerosFibo = new ArrayList<>();
+        int n;
+
+        System.out.print("Insira um número: ");
+        n = lerNum.nextInt();
+
+
+        int cont = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(i < 2) {
+                numerosFibo.add(i);
+            }
+            else {
+                numerosFibo.add(numerosFibo.get(cont) + numerosFibo.get(cont + 1));
+                cont++;
+            }
+        }
+
+        System.out.println(numerosFibo);
+
+
+    }
+
+}
